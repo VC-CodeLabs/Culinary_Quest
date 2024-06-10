bfee852c80ce8270ad90cfdb7a4a13ae86f19c36
VividCloud <154607214+VC-CodeLabs@users.noreply.github.com>
2024-06-10 08:10:45 -0400

    Delete lib directory

--- lib/food.ex
+++
@@ -1,3 +0,0 @@
-defmodule Food do
-  defstruct name: "", cost: 0, satisfaction: 0, category: "", ingredients: [], calories: 0
-end
--- lib/user_constraints.ex
+++
@@ -1,3 +0,0 @@
-defmodule UserConstraints do
-  defstruct budget: 0, allergies: [], calorie_limit: 0
-end
